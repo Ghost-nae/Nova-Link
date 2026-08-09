61a390138e08a1983ec1bd623e3dead97676adc4
Nkosinathi <78370347+Ghost-nae@users.noreply.github.com>
2026-08-09 14:13:12 +0200

    Implement GlobalExceptionHandler for error handling
    This class handles global exceptions for the application, providing structured responses for IllegalArgumentException, IllegalStateException, and RuntimeException.

---
+++ Nova-Link/Nova-Link/src/main/java/com/nova/Nova_Link/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,63 @@
+package com.nova.Nova_Link.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+  
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<Map<String, Object>> handleIllegalArgumentException(
+    IllegalArgumentException exception) {
+
+    Map<String, Object> response = new HashMap<>();
+
+     response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("error", "Bad Request");
+        response.put("message", exception.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(response);
+    
+    }
+
+  @ExceptionHandler(IllegalStateException.class)
+  public ResponseEntity<Map<String, Object>> handleIllegalStateException(
+            IllegalStateException exception) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("error", "Bad Request");
+        response.put("message", exception.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(response);
+    }
+
+  @ExceptionHandler(RuntimeException.class)
+   public ResponseEntity<Map<String, Object>> handleRuntimeException(
+            RuntimeException exception) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.put("error", "Internal Server Error");
+        response.put("message", exception.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    } 
+}
