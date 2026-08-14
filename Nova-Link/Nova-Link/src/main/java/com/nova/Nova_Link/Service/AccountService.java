f472a6c1de1a4bca81b11531bcf6e81eadf0313a
Nkosinathi <78370347+Ghost-nae@users.noreply.github.com>
2026-08-14 22:40:50 +0200

    Remove UserServiceImpl from AccountService
    Removed UserServiceImpl class and its implementation from AccountService. A standalone UserServiceImpl class already exists

--- Nova-Link/Nova-Link/src/main/java/com/nova/Nova_Link/Service/AccountService.java
+++ Nova-Link/Nova-Link/src/main/java/com/nova/Nova_Link/Service/AccountService.java
@@ -79,27 +79,4 @@ public class AccountService {
        }
        accountRepository.save(account);
    }
-
-
-    @Service
-    @RequiredArgsConstructor
-    public static class UserServiceImpl implements UserDetailsService {
-
-        private final UserRepository userRepository;
-
-        @Override
-        public UserDetails loadUserByUsername(String email)
-                throws UsernameNotFoundException {
-
-            User user = userRepository.findByEmail(email)
-                    .orElseThrow(() ->
-                            new UsernameNotFoundException("User not found"));
-
-            return new org.springframework.security.core.userdetails.User(
-                    user.getEmail(),
-                    user.getPassword(),
-                    List.of(new SimpleGrantedAuthority(user.getRole().name()))
-            );
-        }
-    }
 }
