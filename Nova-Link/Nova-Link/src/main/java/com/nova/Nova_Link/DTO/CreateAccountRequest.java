7c02dd09eda5f9c90724ed1b7da9a6728a078384
Nkosinathi <78370347+Ghost-nae@users.noreply.github.com>
2026-08-15 02:36:19 +0200

    Add JsonIgnoreProperties annotation to DTO

--- Nova-Link/Nova-Link/src/main/java/com/nova/Nova_Link/DTO/CreateAccountRequest.java
+++ Nova-Link/Nova-Link/src/main/java/com/nova/Nova_Link/DTO/CreateAccountRequest.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 @Setter
 public class CreateAccountRequest {
