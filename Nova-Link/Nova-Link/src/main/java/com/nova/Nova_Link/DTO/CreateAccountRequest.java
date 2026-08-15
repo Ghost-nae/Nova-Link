5e7e131190017ce83ff730848681846c5281bc7f
Nkosinathi <78370347+Ghost-nae@users.noreply.github.com>
2026-08-15 02:40:15 +0200

    Add size validation to account holder name

--- Nova-Link/Nova-Link/src/main/java/com/nova/Nova_Link/DTO/CreateAccountRequest.java
+++ Nova-Link/Nova-Link/src/main/java/com/nova/Nova_Link/DTO/CreateAccountRequest.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Size;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
@@ -13,6 +14,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class CreateAccountRequest {
 
     @NotBlank(message = "Account holder name is required")
+    @Size(max = 100, message = "Account holder name must not exceed 100 characters")
     private String accountHolderName;
     @NotNull(message = "Account type is required")
     private AccountType type;
