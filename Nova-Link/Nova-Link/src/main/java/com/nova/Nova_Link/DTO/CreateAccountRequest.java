b134e6e3877faa1756d0883508d9162999d9a83a
Nkosinathi <78370347+Ghost-nae@users.noreply.github.com>
2026-08-15 02:27:51 +0200

    Add validation annotations to CreateAccountRequest

--- Nova-Link/Nova-Link/src/main/java/com/nova/Nova_Link/DTO/CreateAccountRequest.java
+++ Nova-Link/Nova-Link/src/main/java/com/nova/Nova_Link/DTO/CreateAccountRequest.java
@@ -3,11 +3,15 @@ package com.nova.Nova_Link.DTO;
 import com.nova.Nova_Link.ENUMS.AccountType;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class CreateAccountRequest {
 
+    @NotBlank(message = "Account holder name is required")
     private String accountHolderName;
+    @NotNull(message = "Account type is required")
     private AccountType type;
 }
